c7a95a407027ee7e3a22811197fbe1c45cf42466
Lia Skalkos <hello@liaskalkos.net>
2015-07-13 10:17:26 -0400

    Complete

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+
+class Prime
+
+  def initialize(nth_element)
+    @nth_element = nth_element
+  end
+
+  def number
+    n=0
+    counter=0
+    prime=nil
+
+    while counter <= @nth_element
+
+      if n.prime?
+        counter +=1
+        prime = n if counter == @nth_element
+      end
+
+      n+=1
+
+    end
+
+    prime
+  end
+
+end
